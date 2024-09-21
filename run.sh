8d9ee705730fd1200b37f8adbf43830eb9acea81
Kamlesh Rawat <kamleshrawat63@gmail.com>
2024-09-21 18:03:54 +0000

    changes

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DEPLOYMENT_FOLDER='<deployment_folder>'
+DEPLOYMENT_FOLDER='/opt/example'
 
 source $DEPLOYMENT_FOLDER/venv/bin/activate
 
 gunicorn app:application \
-         --bind 127.0.0.1:<port> \
+         --bind 127.0.0.1:5000 \
          --workers 2 \
          --threads 2
